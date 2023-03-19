e284b4f4e98a61ab23a3eb252d931bf0c838465d
PuspitaKartika <belajarpuspita4@gmail.com>
2023-03-19 10:34:08 +0700

    container

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,136 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title : "Mentoring Beginner FLutter",
       home: Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.arrow_back),
+          ),
           title: Text("Mentoring Beginner Flutter"),
+          actions: [
+            Icon(Icons.search),
+          ],
         ),
-        body: Center(
-          child: Text("Hello world!", style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-            color: Colors.red
-          ),),
-        ),
+        body: Row(
+          children: [
+            Column(
+              children: [
+                //BIASA
+                Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(10),
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                  child: Text(
+                    "Hiiii", style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+
+                //circle
+                Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(10),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                  child: Text(
+                    "Hiiii", style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                  ),
+                  ),
+                ),
+
+                //radius
+                Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(10),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Hiiii", style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(10),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      width: 5,
+                      color: Colors.green
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Hiiii", style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                  ),
+                ),
+
+
+              ],
+            ),
+
+            Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(10),
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(30,10)
+
+                      )
+                    ]
+                  ),
+                  child: Text(
+                    "Hiiii", style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                  ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
       ),
     );
   }
