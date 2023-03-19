5dd3124120f35add2cc4e9413aa4cd9b74e11fe8
PuspitaKartika <belajarpuspita4@gmail.com>
2023-03-19 11:56:57 +0700

    text input

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +16,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String bulan = 'Januari';
   List<String> kalender = ['Januari', 'Februari', 'Maret'];
+  bool _isTextInput = true; //ini untuk switch
+  String? selectedOption;  //ini untuk radio
+
+  bool option1 = false; //ini untuk checkbox
+  bool option2 = false;//ini untuk checkbox
 
   @override
   Widget build(BuildContext context) {
@@ -33,48 +40,117 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
+            //text field
             Container(
               margin: EdgeInsets.all(30),
-              width: double.infinity,
-              //elevatedbutton
-              child: ElevatedButton(
-                onPressed: (){},
-                child: Text("tombol"),
+              height: 60,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.green
+                )
               ),
-            ),
-            Container(
-              margin: EdgeInsets.all(30),
-              child: TextButton(
-                onPressed: (){},
-                child: Text ("lupa password"),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "masukan e-mail",hintStyle: TextStyle(
+                    color: Colors.grey),
+
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.all(30),
-              child: OutlinedButton(
-                onPressed: (){},
-                child: Text ("lupa password"),
-              ),
-            ),
-            Container(
-              child: IconButton(onPressed: (){},
-                  icon: Icon(Icons.point_of_sale, color: Colors.green,)),
             ),
 
-           // drop down
-            DropdownButton<String>(
-              value: bulan,
-              onChanged: (newValue){
-                setState(() {
-                  bulan = newValue!;
-                });
-              },
-              items: kalender.map<DropdownMenuItem<String>>((String value){
-                return DropdownMenuItem<String>(
-                  value: value,
-                    child: Text(value));
-              }).toList(),
-               )
+            //switch
+            Container(
+              margin: EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Contoh switch"),
+                  Row(
+                    children: [
+                      Switch(
+                        value: _isTextInput, // nilai boolean dari switch
+                        onChanged: (value) {
+                          setState(() {
+                            _isTextInput = value;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      _isTextInput
+                          ? Text("member")
+                          : Text("nonmember"
+
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            //Radio
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Contoh Radio"),
+                  ListTile(
+                    title: const Text('Option 1'),
+                    leading: Radio(
+                      value: 'option1',
+                      groupValue: selectedOption,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedOption = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Option 2'),
+                    leading: Radio(
+                      value: 'option2',
+                      groupValue: selectedOption,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedOption = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //CheckBox
+            Column(
+              children: <Widget>[
+                CheckboxListTile(
+                  title: const Text('Option 1'),
+                  value: option1,
+                  onChanged: (value) {
+                    setState(() {
+                      option1 = value!;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Option 2'),
+                  value: option2,
+                  onChanged: (value) {
+                    setState(() {
+                      option2 = value!;
+                    });
+                  },
+                ),
+
+              ],
+            ),
+
+
           ],
         ),
 
