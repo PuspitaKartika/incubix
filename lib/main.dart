df2f6a45d81cf65b2afe3940ac86dc662db6eca3
PuspitaKartika <belajarpuspita4@gmail.com>
2023-05-27 17:29:24 +0700

    configure with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
@@ -6,7 +7,13 @@ import 'package:incubix/pages/login.dart';
 import 'package:incubix/pages/register.dart';
 import 'package:incubix/pages/splash.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
