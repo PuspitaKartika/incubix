f5fb7bd5fe193aff7d514e89cce42aae975d212d
PuspitaKartika <belajarpuspita4@gmail.com>
2023-06-04 20:26:04 +0700

    authentication witg google

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:incubix/data/data.dart';
 import 'package:incubix/pages/detail.dart';
+import 'package:incubix/pages/login.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,79 +18,96 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 10),
           height: 40,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Color(0xffF1F1F1)
-          ),
+              borderRadius: BorderRadius.circular(10),
+              color: Color(0xffF1F1F1)),
           child: Center(
             child: TextField(
-              onChanged: (value){},
+              onChanged: (value) {},
               decoration: InputDecoration.collapsed(hintText: 'search'),
             ),
           ),
         ),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                        (route) => false));
+              },
+              icon: Icon(
+                Icons.logout,
+                color: Colors.black,
+              ))
+        ],
       ),
       body: ListView.builder(
-        itemCount: items.length,
-          itemBuilder: (BuildContext context, int index){
-        return GestureDetector(
-          onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => DetailCard(index: index)));
-          },
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.2),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0,3)
-                )
-              ]
-            ),
-            child: Column(
-              children: [
-                Container(
-                  height: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topRight: Radius.circular(10), topLeft: Radius.circular(10)),
-                    image: DecorationImage(
-                      image: AssetImage(items[index]['image']),
-                      fit: BoxFit.cover
-                    )
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10))
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(items[index]['text'], style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),),
-                      SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailCard(index: index)));
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3))
+                ]),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              topLeft: Radius.circular(10)),
+                          image: DecorationImage(
+                              image: AssetImage(items[index]['image']),
+                              fit: BoxFit.cover)),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10))),
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("Alamat"),
-                          Text(items[index]['address'])
+                          Text(
+                            items[index]['text'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Alamat"),
+                              Text(items[index]['address'])
+                            ],
+                          )
                         ],
-                      )
-                    ],
-                  ),
-
-                )
-
-              ],
-            ),
-
-          ),
-        );
-      }),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:incubix/bloc/login/login_cubit.dart';
+import 'package:incubix/pages/home.dart';
 import 'package:incubix/utils/routes.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,6 +17,20 @@ class _LoginPageState extends State<LoginPage> {
   final emailEdc = TextEditingController();
   final passEdc = TextEditingController();
   bool passInvisible = true;
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential).then(
+        (value) async => await Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+            (route) => false));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -215,6 +232,35 @@ class _LoginPageState extends State<LoginPage> {
               inputEmail(),
               inputPassword(),
               login(),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      signInWithGoogle();
+                    },
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                          'https://img2.pngdownload.id/20190228/qby/kisspng-google-logo-google-account-g-suite-google-images-g-icon-archives-search-png-5c77ad39b77471.9286340315513470017515.jpg'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                          'https://freepngimg.com/thumb/business/83615-blue-icons-symbol-telephone-computer-logo.png'),
+                    ),
+                  )
+                ],
+              ),
               SizedBox(
                 height: 180,
               ),
